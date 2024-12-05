dc9b57be4b8781d6eca208aa563b39a423dedb8a
Шагелди <129554782+Shageldi17@users.noreply.github.com>
2024-12-06 00:07:41 +0300

    first commit

---
+++ src/main/java/_997recursion/NeChotnyChisel.java
@@ -0,0 +1,9 @@
+package _997recursion;
+
+public class NeChotnyChisel {
+    public static void main (String[] args ){
+        for (int i = 1; i < 100; i +=2  ){
+            System.out.println(i);
+        }
+    }
+}
--- src/main/java/_997recursion/RecursionFactorial.java
+++ src/main/java/_997recursion/RecursionFactorial.java
@@ -3,7 +3,7 @@ package _997recursion;
 public class RecursionFactorial {
     public static int factorial (int n){
         if (n == 0 || n == 1){
-            return 1 ;
+            return 1;
         }
         return n * factorial(n-1);
     }
