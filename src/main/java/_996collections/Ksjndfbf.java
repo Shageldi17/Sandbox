bb5e30237626b8c13b4fb26414518bbd80d37ed5
Шагелди <129554782+Shageldi17@users.noreply.github.com>
2024-12-08 23:05:43 +0300

    second commit

--- src/main/java/_996collections/Ksjndfbf.java
+++ src/main/java/_996collections/Ksjndfbf.java
@@ -2,8 +2,8 @@ package _996collections;
 
 public class Ksjndfbf {
     public static void main(String[] args) {
-        for(int i = 0; i < 13; ++i) {
+        for (int i = 0; i < 13; ++i) {
             System.out.println("PrintGreeting");
         }
-
     }
+}
