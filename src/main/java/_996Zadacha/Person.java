646a55a20b85adaeee63902df261bfa9eaa8e625
Шагелди <129554782+Shageldi17@users.noreply.github.com>
2024-12-11 19:48:06 +0300

    second commit

---
+++ src/main/java/_996Zadacha/Person.java
@@ -0,0 +1,15 @@
+package _996Zadacha;
+
+public class Person {
+    private String name;
+    private String age;
+    private String email;
+    private String phoneNumber;
+
+    public void setName(String name){ this.name = name;}
+    public void setAge (int age){ this.age = age;}
+    public void setEmail(String email){this.email = email;}
+    public void setPhoneNumber( )
+
+
+}
